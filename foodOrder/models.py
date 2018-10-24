e692d9bc4177dc959707af94f5ea14fdbf60c237
Qurban Ali <qurban.ali36@yahoo.com>
2018-10-24 11:51:04 +0500

    working on foodOrder

--- foodOrder/models.py
+++ foodOrder/models.py
@@ -4,15 +4,15 @@ class Food(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     vendor = models.CharField(max_length=50)
+    isActive = models.BooleanField(verbose_name='Active', default=True)
     isBreakfast = models.BooleanField(verbose_name='Breakfast')
     isLunch = models.BooleanField(verbose_name='Lunch')
     isDinner = models.BooleanField(verbose_name='Dinner')
     isSehri = models.BooleanField(verbose_name='Sehri')
     isAftari = models.BooleanField(verbose_name='Aftari')
-    isActive = models.BooleanField(verbose_name='Active')
     
     def __str__(self):
-        return self.name +' - '+ str(self.price)
+        return self.name +' - '+ str(self.vendor) +' - '+ str(self.price)
 
 class FoodOrder(models.Model):
     SERVED = 'served'
@@ -27,12 +27,26 @@ class FoodOrder(models.Model):
                       (PENDING, 'Pending'),
                       (PLACED, 'Placed'))
     
-    food = models.ForeignKey(Food, null=True, blank=True, on_delete=models.SET_NULL)
-    employee = models.ForeignKey('home.Employee', related_name='foods', null=True, blank=True, on_delete=models.SET_NULL)
-    project = models.ForeignKey('home.Project', null=True, blank=True, on_delete=models.SET_NULL)
-    datetime = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=15)
-    approvedBy = models.ForeignKey('home.Employee', related_name='approved_foods', null=True, blank=True, on_delete=models.SET_NULL)
+    food = models.ForeignKey(Food, null=True, blank=True,
+                             on_delete=models.SET_NULL)
+    employee = models.ForeignKey('home.Employee', related_name='foods',
+                                 null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    project = models.ForeignKey('home.Project', null=True, blank=True,
+                                on_delete=models.SET_NULL)
+    orderTime = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=15,
+                              default=PLACED)
+    approvedBy = models.ForeignKey('home.Employee',
+                                   related_name='approved_foods', null=True,
+                                   blank=True, on_delete=models.SET_NULL)
+    approvalTime = models.DateTimeField(null=True)
     
     def __str__(self):
-        return self.employee.name +' - '+ self.food.name +' - '+ str(self.datetime)
+        return (self.employee.name +' - '+ self.food.name +' - '+
+                str(self.datetime))
+    
+    def nice_status(self):
+        for status, nice_status in FoodOrder.STATUS_CHOICES:
+            if status == self.status:
+                return nice_status
--- foodOrder/views.py
+++ foodOrder/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . import models
+from home.views import loggedInUser
+from home.models import Project
 
-# Create your views here.
-def home(*args):
-    return HttpResponse('Food Order')
+def home(request):
+    user = loggedInUser(request)
+    if user:
+        context = {'user': user}
+        context['foods'] = models.Food.objects.filter(isActive=True)
+        context['projects'] = Project.objects.filter(active=True)
+        if request.method == "GET":
+            return render(request, 'foodOrder/order.html', context)
+        else:
+            food = request.POST.get('food')
+            project = request.POST.get('project')
+            food = models.Food.objects.get(pk=food)
+            project = Project.objects.get(pk=project)
+            #models.FoodOrder.objects.get(employee=user, food=food, project=project)
+            return render(request, 'foodOrder/order.html', context)
+    else:
+        redirect('/login')
